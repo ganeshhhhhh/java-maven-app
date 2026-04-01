23e44b23769d9be52f0914167df679461c16c26a
ganesh prasad maity <72559039+ganeshhhhhh@users.noreply.github.com>
2026-04-01 13:53:50 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,10 @@
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello DevOps");
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello DevOps 🚀";
     }
 }
-
